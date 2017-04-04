02d663bda494318a5da343ccbcd0b59079a93a31
Jakubinyi <jakubinyi@gmail.com>
2017-04-04 20:03:47 +0200

    add/refactor exception handling

--- src/Server.java
+++ src/Server.java
@@ -1,28 +1,76 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by jakubinyi on 2017.04.04..
  */
 public class Server {
 
-      public void run() throws Exception {
-          ServerSocket serverSocket = new ServerSocket(6789);
-          Socket socket = serverSocket.accept();
-          ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-          Document doc = (Document)in.readObject();
-          System.out.println("Recieved document!");
-      }
+    public void run() {
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
 
-    public static void main(String[] args) {
         try {
-            Server server = new Server();
-            server.run();
-        } catch (Exception e) {
+            ServerSocket serverSocket = new ServerSocket(6789);
+            Socket socket = serverSocket.accept();
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+            Document doc = (Document) in.readObject();
+            System.out.println("Recieved document!");
+
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getDocumentElement().getElementsByTagName("rate");
+            for (int temp = 0; temp < nodeList.getLength(); temp++) {
+                Node nNode = nodeList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    dataset.addValue(Integer.parseInt(eElement.getAttribute("y")), "Energy", eElement.getAttribute("x"));
+                }
+            }
+
+            in.close();
+            creatChart(dataset);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("Class not found!");
+        } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+      }
+
+      public void creatChart(DefaultCategoryDataset dataset) throws IOException {
+          DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+          LocalDate localDate = LocalDate.now();
+          String date = DateTimeFormatter.ofPattern("yyy/MM/dd").format(localDate);
+
+          JFreeChart lineChartObject = ChartFactory.createLineChart(
+                  date,"Hour","W/h",
+                  dataset, PlotOrientation.VERTICAL,
+                  true,true,false);
+
+          int width = 800; /* Width of the image */
+          int height = 600; /* Height of the image */
+          File lineChart = new File( "src/LineChart.jpeg" );
+          ChartUtilities.saveChartAsJPEG(lineChart ,lineChartObject, width ,height);
+      }
+
+      public static void main(String[] args) {
+            Server server = new Server();
+            server.run();
+      }
 }
